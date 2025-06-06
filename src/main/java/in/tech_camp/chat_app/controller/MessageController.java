a05833351f49f9f981cc9aee2e96a47f96e81bf3
Ganta11 <t.iwahara@infomart.co.jp>
2025-06-06 11:51:05 +0900

    サイドバーとチャット画面ファイルの分割

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MessageController {
   @GetMapping("/")
-  public String showMesaages(){
+  public String showMessages(){
       return "messages/index";
   }
 }
